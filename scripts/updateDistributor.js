9104e48182bc4323c054040524c54611de3463df
Phi <phi@lattefi.app>
2021-06-09 11:16:31 +0800

    update scripts

--- scripts/updateDistributor.js
+++ scripts/updateDistributor.js
@@ -13,23 +13,31 @@ async function main() {
   const uniFarm = await contractAt("Farm", "0x8A3d7A49ADa4FED00d83fA6827F46F83b62eF87F")
   const burnVaultV2 = await contractAt("BurnVault", "0x780e9996Ec934cba0E2FC830C9b9f3e19F99ec3B")
   const timeVaultV2 = await contractAt("TimeVault", "0x82147C5A7E850eA4E28155DF107F2590fD4ba327")
+  const floor = { address: "0x40ED3699C2fFe43939ecf2F3d11F633b522820aD" }
 
-  const totalRewards = "3"
+  const totalRewards = "6.7"
   const totalRewardsAmount = ethers.utils.parseUnits(totalRewards, tokenDecimals)
-  const transferAmout = totalRewardsAmount.mul(9500).div(10000)
+  const transferAmoutForDistributor = totalRewardsAmount.mul(8000).div(10000)
+  const transferAmoutForFloor = totalRewardsAmount.mul(2000).div(10000)
 
-  console.log("transferAmout", ethers.utils.formatUnits(transferAmout, tokenDecimals))
+  console.log("transferAmoutForDistributor", ethers.utils.formatUnits(transferAmoutForDistributor, tokenDecimals))
   await sendTxn(wallet.sendTransaction({
     to: distributor.address,
-    value: transferAmout
-  }), "send ETH")
+    value: transferAmoutForDistributor
+  }), "transfer ETH")
+
+  console.log("transferAmoutForFloor", ethers.utils.formatUnits(transferAmoutForFloor, tokenDecimals))
+  await sendTxn(wallet.sendTransaction({
+    to: floor.address,
+    value: transferAmoutForFloor
+  }), "transfer ETH")
 
   const hourlyRewards = totalRewardsAmount.div(168)
 
   const xlgeRewards = hourlyRewards.mul(3000).div(10000)
   const uniFarmRewards = hourlyRewards.mul(5000).div(10000)
-  const burnVaultRewards = hourlyRewards.mul(1000).div(10000)
-  const timeVaultRewards = hourlyRewards.mul(500).div(10000)
+  const burnVaultRewards = hourlyRewards.mul(0)
+  const timeVaultRewards = hourlyRewards.mul(0)
 
   console.log("rewards", xlgeRewards.toString(), uniFarmRewards.toString(), burnVaultRewards.toString(), timeVaultRewards.toString())
 
