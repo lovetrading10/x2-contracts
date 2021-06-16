239a5742e439c368a9d3b2dc22110967b449ecaa
Phi <phi@lattefi.app>
2021-06-16 10:39:43 +0800

    update scripts

--- scripts/updateDistributor.js
+++ scripts/updateDistributor.js
@@ -11,11 +11,9 @@ async function main() {
 
   const xlgeFarm = await contractAt("Farm", "0x7c1cFFEA9E773186A5950Bb27CBf14b895b4025E")
   const uniFarm = await contractAt("Farm", "0x8A3d7A49ADa4FED00d83fA6827F46F83b62eF87F")
-  const burnVaultV2 = await contractAt("BurnVault", "0x780e9996Ec934cba0E2FC830C9b9f3e19F99ec3B")
-  const timeVaultV2 = await contractAt("TimeVault", "0x82147C5A7E850eA4E28155DF107F2590fD4ba327")
   const floor = { address: "0x40ED3699C2fFe43939ecf2F3d11F633b522820aD" }
 
-  const totalRewards = "6.7"
+  const totalRewards = "2.2"
   const totalRewardsAmount = ethers.utils.parseUnits(totalRewards, tokenDecimals)
   const transferAmoutForDistributor = totalRewardsAmount.mul(8000).div(10000)
   const transferAmoutForFloor = totalRewardsAmount.mul(2000).div(10000)
@@ -36,26 +34,18 @@ async function main() {
 
   const xlgeRewards = hourlyRewards.mul(3000).div(10000)
   const uniFarmRewards = hourlyRewards.mul(5000).div(10000)
-  const burnVaultRewards = hourlyRewards.mul(0)
-  const timeVaultRewards = hourlyRewards.mul(0)
 
-  console.log("rewards", xlgeRewards.toString(), uniFarmRewards.toString(), burnVaultRewards.toString(), timeVaultRewards.toString())
+  console.log("rewards", xlgeRewards.toString(), uniFarmRewards.toString())
 
   await sendTxn(xlgeFarm.claim(account.address), "xlgeFarm.claim")
   await sendTxn(uniFarm.claim(account.address), "uniFarm.claim")
-  await sendTxn(burnVaultV2.claim(account.address), "burnVaultV2.claim")
-  await sendTxn(timeVaultV2.claim(account.address), "timeVaultV2.claim")
 
   await sendTxn(distributor.setDistribution([
     xlgeFarm.address,
-    uniFarm.address,
-    burnVaultV2.address,
-    timeVaultV2.address
+    uniFarm.address
   ], [
     xlgeRewards,
-    uniFarmRewards,
-    burnVaultRewards,
-    timeVaultRewards
+    uniFarmRewards
   ]), "distributor.setDistribution")
 
   return { distributor }
